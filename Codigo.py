b79b771842d65cc9787d1531914d9004ae131a69
Karalos <carloseas1@al.insper.edu.br>
2020-04-01 17:16:14 -0300

    Conceitos Basicos

---
+++ Codigo.py
@@ -0,0 +1,22 @@
+#imports
+import math
+import random
+
+#conceitos basicos do codigo
+#dados
+n1=random.randint(1,6)
+n2=random.randint(1,6)
+soma=n1+n2
+
+#fases
+fc="Come Out"
+fp="Point"
+
+#fichas
+f=100
+
+#apostas
+PLB="Pass Line Belt"
+F="Field"
+AC="Any Craps"
+T="Twelve"
